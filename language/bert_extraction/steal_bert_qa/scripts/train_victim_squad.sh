bf8d0847e08581d6f0810d84d2af7be70eb0d74c
naveenjafer <f2013019@hyderabad.bits-pilani.ac.in>
2020-05-07 01:56:31 +0530

    fixes compat.v1 issue and typo in flag

--- language/bert_extraction/steal_bert_qa/scripts/train_victim_squad.sh
+++ language/bert_extraction/steal_bert_qa/scripts/train_victim_squad.sh
@@ -21,41 +21,37 @@ export BERT_DIR=/path/to/bert/uncased_L-24_H-1024_A-16
 export SQUAD_DIR=/path/to/squad
 export OUTPUT_DIR=/path/to/output_dir
 
-export BERT_DIR=/home/naveen/scratch/google-language-fork/bertModelDir/uncased_L-4_H-256_A-4
-export SQUAD_DIR=/home/naveen/scratch/google-language-fork/squadDir
-export OUTPUT_DIR=/home/naveen/scratch/google-language-fork/outputDir
-
 # STEP 1
 # Download the SQuAD datasets. This is a one-time step, can be ignored once done.
-#wget -O $SQUAD_DIR/train-v1.1.json https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json
-#wget -O $SQUAD_DIR/train-v2.0.json https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
-#wget -O $SQUAD_DIR/dev-v1.1.json https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json
-#wget -O $SQUAD_DIR/dev-v2.0.json https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
+wget -O $SQUAD_DIR/train-v1.1.json https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json
+wget -O $SQUAD_DIR/train-v2.0.json https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
+wget -O $SQUAD_DIR/dev-v1.1.json https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json
+wget -O $SQUAD_DIR/dev-v2.0.json https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
 
 # STEP 2
 # Train the victim model
-#python -m language.bert_extraction.steal_bert_qa.models.run_squad \
-#  --exp_name="train_victim_squad" \
-#  --version_2_with_negative=false \
-#  --do_train=true \
-#  --do_predict=true \
-#  --do_lower_case=true \
-#  --save_checkpoints_steps=5000 \
-#  --train_file=$SQUAD_DIR/train-v1.1.json \
-#  --predict_input_file=$SQUAD_DIR/dev-v1.1.json \
-#  --predict_output_dir=$OUTPUT_DIR \
-#  --vocab_file=$BERT_DIR/vocab.txt \
-#  --bert_config_file=$BERT_DIR/bert_config.json \
-#  --init_checkpoint=$BERT_DIR/bert_model.ckpt \
-#  --max_seq_length=384 \
-#  --train_batch_size=32 \
-#  --learning_rate=5e-5 \
-#  --num_train_epochs=3.0 \
-#  --output_dir=$OUTPUT_DIR
+python -m language.bert_extraction.steal_bert_qa.models.run_squad \
+  --exp_name="train_victim_squad" \
+  --version_2_with_negative=false \
+  --do_train=true \
+  --do_predict=true \
+  --do_lower_case=true \
+  --save_checkpoints_steps=5000 \
+  --train_file=$SQUAD_DIR/train-v1.1.json \
+  --predict_input_file=$SQUAD_DIR/dev-v1.1.json \
+  --predict_output_dir=$OUTPUT_DIR \
+  --vocab_file=$BERT_DIR/vocab.txt \
+  --bert_config_file=$BERT_DIR/bert_config.json \
+  --init_checkpoint=$BERT_DIR/bert_model.ckpt \
+  --max_seq_length=384 \
+  --train_batch_size=32 \
+  --learning_rate=5e-5 \
+  --num_train_epochs=3.0 \
+  --output_dir=$OUTPUT_DIR
 
-# STEP 3
-# Evaluate the predictions of the victim model using the SQuAD eval script
-# For SQuAD 2.0, use the script language.bert_extraction.steal_bert_qa.utils.evaluate_squad_2
+ STEP 3
+ Evaluate the predictions of the victim model using the SQuAD eval script
+ For SQuAD 2.0, use the script language.bert_extraction.steal_bert_qa.utils.evaluate_squad_2
 python -m language.bert_extraction.steal_bert_qa.utils.evaluate_squad \
   --dataset_file=$SQUAD_DIR/dev-v1.1.json \
   --prediction_file=$OUTPUT_DIR/predictions.json
