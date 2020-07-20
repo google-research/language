39c858f7d76de1571204859a562e5392a30daaf3
Alane Suhr <alsuhr@gmail.com>
2020-07-20 16:14:19 -0400

    Timing evaluation

--- language/xsp/evaluation/official_evaluation.py
+++ language/xsp/evaluation/official_evaluation.py
@@ -36,7 +36,7 @@ import timeout_decorator
 from tqdm import tqdm
 
 # Maximum allowable timeout for executing predicted and gold queries.
-TIMEOUT = 15
+TIMEOUT = 60
 
 # Maximum number of candidates we should consider
 MAX_CANDIDATE = 20
@@ -93,6 +93,7 @@ def try_executing_query(prediction, cursor, case_sensitive=True, verbose=False):
   prediction_str = prediction_str.replace(';', '').strip()
   print('Current prediction:' + prediction_str)
 
+  st = time.time()
   try:
     if not case_sensitive:
       new_prediction = ''
@@ -119,8 +120,9 @@ def try_executing_query(prediction, cursor, case_sensitive=True, verbose=False):
           sqlite3.OperationalError, sqlite3.NotSupportedError) as e:
     exception_str = str(e).lower()
     pred_results = []
+  execution_time = time.time() - st
 
-  return pred_results, exception_str
+  return pred_results, exception_str, execution_time
 
 
 @timeout_decorator.timeout(seconds=TIMEOUT, use_signals=False)
@@ -220,6 +222,7 @@ def execute_prediction(prediction, empty_table_cursor, cursor, case_sensitive,
   best_prediction = None
   pred_results = None
   exception_str = None
+  execution_time = 0
 
   if len(sorted_by_scores) > MAX_CANDIDATE:
     sorted_by_scores = sorted_by_scores[:MAX_CANDIDATE]
@@ -241,7 +244,7 @@ def execute_prediction(prediction, empty_table_cursor, cursor, case_sensitive,
         # Get the actual results
         if verbose:
           print('... on actual database')
-        pred_results, exception_str = try_executing_query(
+        pred_results, exception_str, execution_time = try_executing_query(
             pred, cursor, case_sensitive, verbose)
       if exception_str == 'timeout':
         # Technically, this query didn't have a syntax problem, so
@@ -250,7 +253,7 @@ def execute_prediction(prediction, empty_table_cursor, cursor, case_sensitive,
 
         if verbose:
           print('... on actual database')
-        pred_results, exception_str = try_executing_query(
+        pred_results, exception_str, execution_time = try_executing_query(
             pred, cursor, case_sensitive, verbose)
         break
     else:
@@ -259,11 +262,11 @@ def execute_prediction(prediction, empty_table_cursor, cursor, case_sensitive,
 
       if verbose:
         print('No exception... on actual database')
-      pred_results = try_executing_query(pred, cursor, case_sensitive,
-                                         verbose)[0]
+      pred_results, _, execution_time = try_executing_query(pred, cursor, case_sensitive,
+                                         verbose)
       break
 
-  return best_prediction, pred_results, exception_str
+  return best_prediction, pred_results, exception_str, execution_time
 
 
 def _convert_to_unicode_string(value):
@@ -349,7 +352,7 @@ def execute_predictions(predictions, cache_dict, ofile, case_sensitive,
       print('Finding the highest-rated prediction for utterance:\n\t' +
             printable_utterance)
 
-    best_prediction, pred_results, exception_str = execute_prediction(
+    best_prediction, pred_results, exception_str, execution_time = execute_prediction(
         prediction, empty_cursor, cursor, case_sensitive, verbose)
 
     ofile.write('Predicted query:\n')
@@ -358,6 +361,7 @@ def execute_predictions(predictions, cache_dict, ofile, case_sensitive,
                   '\n')
     else:
       ofile.write('ERROR: Cannot write prediction %r\n' % best_prediction)
+    ofile.write('Took %s s to execute' % execution_time)
 
     # If it didn't execute correctly, check why.
     if exception_str:
